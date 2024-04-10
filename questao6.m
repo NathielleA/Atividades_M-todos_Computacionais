3107d09f582b8528970a83d9619cccea495abd2e
Nathielle Alves <telly.cerq@gmail.com>
2024-04-09 22:15:08 -0300

    :arrow_forward: Questão 6

---
+++ questao6.m
@@ -0,0 +1,27 @@
+## Copyright (C) 2024 Nathiele
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn {} {@var{retval} =} questao6 (@var{input1}, @var{input2})
+##
+## @seealso{}
+## @end deftypefn
+
+## Author: Nathiele <Nathiele@DESKTOP-7PQSJJF>
+## Created: 2024-04-09
+
+function retval = questao6 (input1, input2)
+
+endfunction
