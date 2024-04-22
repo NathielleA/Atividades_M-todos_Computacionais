1b59f67dcce3fab10cf827d7ed8a12a4eecaf936
Nathielle Alves <telly.cerq@gmail.com>
2024-04-22 17:22:36 -0300

    Update questao7.m

--- Lista Aula 2 - Aproximação e Erro/questao7.m
+++ Lista Aula 2 - Aproximação e Erro/questao7.m
@@ -1,30 +1,25 @@
 function [ii, f, trueError] = questao7(x, h, trueValue)
 
   %Estimando a derivada primeira da função fx
-  %x = 0.5;
+  x = 0.5;
   h = 1;
   trueValue = -0.9125; %Valor verdadeiro (cálculo na mão)
-
-  df = @(x) -0.4*x.^3 - 0.45*x.^2 - x - 0.25;
-  trueDerivate = df(0.5);
 
   xGrf = [];
   yGrf = [];
 
-  disp("Tamanho do passo | Diferença finita | Erro verdadeiro");
+  fprintf("Tamanho do passo | diferença finita | erro verdadeiro\n");
 
   for ii = 0:11
-    f = (func(0.5 + h) - func(0.5 - h))/(2*h);
+    f = (func(x + h) - func(x - h))/2*h;
     h = h/10;
     trueError = trueValue - f;
+    fprintf("        %i        |   %e  |      %f\n", ii, f, trueError);
 
-    % Tabela de resultados
-    fprintf('       %9d | %.14f | %.14f\n', ii, f, trueError);
-
-    xGrf(ii + 1) = h;
-    yGrf(ii + 1) = abs(trueError);
+    xGrf(ii + 1) = ii;
+    yGrf(ii + 1) = trueError;
   endfor
 
-  loglog(xGrf, yGrf, "-");
+  plot(xGrf, yGrf, "-");
 
 endfunction
